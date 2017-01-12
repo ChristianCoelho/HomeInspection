295a9ec5e3c1d4a0e8ef785020f1b14108cb03f3
Jared Speck <jspeck@csc235x03.ad.calpoly.edu>
2017-01-11 18:25:14 -0800

    Initial Commit for branch. Added Skeleton classes for inspection info view

---
+++ HomeInspection/HomeInspection/InspInfoView.swift
@@ -0,0 +1,14 @@
+//
+//  InspInfoView.swift
+//  HomeInspection
+//
+//  Created by Jared Speck on 1/11/17.
+//  Copyright © 2017 Jared Speck. All rights reserved.
+//
+
+import UIKit
+
+class InspInfoView: UIView {
+    
+    
+}
