635c6e1af0b806f8a52b9964d079757833a9c671
Jared Speck <jspeck@csc235x03.ad.calpoly.edu>
2017-01-11 18:36:39 -0800

    Testing git pushing. Local vs remote master branch

--- HomeInspection/HomeInspection/DashboardViewController.swift
+++ HomeInspection/HomeInspection/DashboardViewController.swift
@@ -15,6 +15,9 @@ class DashboardViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //Testing
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
