18359d99fac2d37fa8e1362f9841b06fed5b5a0d
Jared Speck <jspeck@csc235x03.ad.calpoly.edu>
2017-01-11 18:50:28 -0800

    Testing

--- HomeInspection/HomeInspection/LookupViewController.swift
+++ HomeInspection/HomeInspection/LookupViewController.swift
@@ -14,6 +14,8 @@ class LookupViewController: UIViewController {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+        //Testing
+        
     }
     
     override func didReceiveMemoryWarning() {
